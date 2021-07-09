03b84b50f444d6b4232bcc25144596dc3038982e
Vamsi <andiboyina.saivamsi@hcl.com>
2021-07-09 12:21:22 +0530

    adding loading animations

---
+++ Pages/Test.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tambola.Pages
+{
+    public partial class Test
+    {
+        private void btnClicked()
+        {
+            Console.WriteLine("btn clicked");
+        }
+    }
+}

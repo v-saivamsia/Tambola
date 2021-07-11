03b80c2cd061fbed3a35ccdd00ea063a72e109f4
Vamsi <andiboyina.saivamsi@hcl.com>
2021-07-11 08:32:09 +0530

    adding ui elements

---
+++ Pages/AddPlayer.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tambola.Pages
+{
+    public partial class AddPlayer
+    {
+        [Parameter]
+        public bool showTickets { get; set; } = false;
+        private string showPanel = "";
+        private async Task closeTicketsPanel()
+        {
+            showTickets = false;
+            await showTicketsPanel.InvokeAsync(!showTickets);
+            Console.WriteLine("close button clicked");
+            //StateHasChanged();
+        }
+        [Parameter]
+        public EventCallback<bool> showTicketsPanel { get; set; } 
+        protected override async Task OnParametersSetAsync()
+        {
+            showPanel = showTickets ? "showPanel" : "";
+            await base.OnParametersSetAsync();
+        }
+    }
+}
--- Pages/Test.razor.cs
+++ Pages/Test.razor.cs
@@ -7,9 +7,19 @@ namespace Tambola.Pages
 {
     public partial class Test
     {
+        private bool showTickets = false;
         private void btnClicked()
         {
+            showTickets = true;
             Console.WriteLine("btn clicked");
+        }
+        private void closeButtonInChildClicked(bool isClosed)
+        {
+            if (isClosed)
+            {
+                showTickets = false;
+                StateHasChanged();
+            }
         }
     }
 }
